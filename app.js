7da4414426f6f84e2575d8cb0ebd116648d44010
jayant <jayantparihar@yahoo.com>
2021-03-12 17:00:56 -0800

    file updated with condition

---
+++ app.js
@@ -0,0 +1,14 @@
+let number_1 = 45
+let string_1 = "JAYANT"
+const number_2 = 45
+const Boolean_1 = true
+
+if (number_1 == number_2) {
+    console.log("they match")
+} else {
+    console.log("no luck")
+}
+
+for (letter of string_1) {
+    console.log(letter)
+}
