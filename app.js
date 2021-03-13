dba1a4af8edb67f6d1b5910a9982e6098639c95e
jayant <jayantparihar@yahoo.com>
2021-03-12 17:09:38 -0800

    updated javascript

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let string_1 = "JAYANT"
 const number_2 = 45
 const Boolean_1 = true
 
-if (number_1 == number_2) {
+if (number_1 === number_2) {
     console.log("they match")
 } else {
     console.log("no luck")
